96d4604b2e96f3448f858baaf84d0650f5237721
Scifiknux <scifiknux@yahoo.com>
2021-03-15 22:58:29 -0600

    Initial Commit

---
+++ RemoteUninstallKB500802.bat
@@ -0,0 +1,98 @@
+cls
+:start
+cls
+@pushd %~dp0
+@echo off
+:top
+ECHO "  ___               __   __    _ _            ___ _____ "
+ECHO " |   \ ___ ___ _ _  \ \ / /_ _| | |___ _  _  |_ _|_   _|"
+ECHO " | |) / -_) -_) '_|  \ V / _` | | / -_) || |  | |  | |  "
+ECHO " |___/\___\___|_|     \_/\__,_|_|_\___|\_, | |___| |_|  "
+ECHO "                                       |__/             "
+ECHO ============================
+ECHO Remote KB500802 Uninstall
+ECHO ============================
+ECHO ____________________________
+color 0A
+set /p "address=Enter IP of computer you want to check: "
+ping -n 1 "%address%" | findstr /r /c:"[0-9] *ms"
+if %errorlevel% == 0 (
+    GOTO:one ) else (
+    echo [41mUnable to find %address%. Device may be offline. Try again?[0m
+    pause
+    GOTO:top
+    )
+:one
+cls
+ECHO "  ___               __   __    _ _            ___ _____ "
+ECHO " |   \ ___ ___ _ _  \ \ / /_ _| | |___ _  _  |_ _|_   _|"
+ECHO " | |) / -_) -_) '_|  \ V / _` | | / -_) || |  | |  | |  "
+ECHO " |___/\___\___|_|     \_/\__,_|_|_\___|\_, | |___| |_|  "
+ECHO "                                       |__/             "
+ECHO ============================
+ECHO Remote KB500802 Uninstall
+ECHO ============================
+ECHO ____________________________
+color 0A
+echo What computer (name or IP) do you want to run this on?
+echo %address%
+ECHO Please wait... Checking system information.
+ECHO ============================
+ECHO OS INFO
+ECHO ============================
+systeminfo /s %address% | findstr /r /c:"KB5000802"
+if %errorlevel% == 0 (
+    GOTO:found ) else (
+    GOTO:notfound
+    )
+:found
+echo Please enter your admin username:
+set /p user=
+xcopy "%~dp0\Files\*.*" \\%address%\c$\Temp\RemoveKB5000802 /e /i /y
+cls
+ECHO "  ___               __   __    _ _            ___ _____ "
+ECHO " |   \ ___ ___ _ _  \ \ / /_ _| | |___ _  _  |_ _|_   _|"
+ECHO " | |) / -_) -_) '_|  \ V / _` | | / -_) || |  | |  | |  "
+ECHO " |___/\___\___|_|     \_/\__,_|_|_\___|\_, | |___| |_|  "
+ECHO "                                       |__/             "
+ECHO ============================
+ECHO Remote KB500802 Uninstall - %address%
+ECHO ============================
+ECHO ____________________________
+echo [41mThis will take a long time. Please do not close this window until the script finishes.[0m
+PSTools\psexec \\%address% -u deervalley\%user% -s PowerShell "C:\Temp\RemoveKB5000802\removeKB5000802.ps1"
+@RD /S /Q "\\%address%\c$\Temp\RemoveKB5000802"
+echo All done!
+pause
+cls
+ECHO "  ___               __   __    _ _            ___ _____ "
+ECHO " |   \ ___ ___ _ _  \ \ / /_ _| | |___ _  _  |_ _|_   _|"
+ECHO " | |) / -_) -_) '_|  \ V / _` | | / -_) || |  | |  | |  "
+ECHO " |___/\___\___|_|     \_/\__,_|_|_\___|\_, | |___| |_|  "
+ECHO "                                       |__/             "
+ECHO ============================
+ECHO Remote KB500802 Uninstall
+ECHO ============================
+ECHO ____________________________
+color 0A
+echo KB50000802 has been successfully uninstalled on %address%.
+@popd
+pause
+GOTO :EOF
+:notfound
+cls
+ECHO "  ___               __   __    _ _            ___ _____ "
+ECHO " |   \ ___ ___ _ _  \ \ / /_ _| | |___ _  _  |_ _|_   _|"
+ECHO " | |) / -_) -_) '_|  \ V / _` | | / -_) || |  | |  | |  "
+ECHO " |___/\___\___|_|     \_/\__,_|_|_\___|\_, | |___| |_|  "
+ECHO "                                       |__/             "
+ECHO ============================
+ECHO Remote KB500802 Uninstall
+ECHO ============================
+ECHO ____________________________
+color 0A
+echo [41mHotfix KB500802 not found. Are you sure it's installed?[0m
+@popd
+pause
+GOTO :start
+:: Love, CJ Pollock
