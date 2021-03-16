06437251273ace7e9debbb3bb77c7341824f9092
Scifiknux <scifiknux@yahoo.com>
2021-03-15 23:04:31 -0600

    Initial Commit

---
+++ Files/removeKB5000802.ps1
@@ -0,0 +1,5 @@
+$SearchUpdates = dism /online /get-packages | findstr "Package_for"
+$updateName = $SearchUpdates.replace("Package Identity : ", "")
+$updates =  $updateName | findstr "19041.867"
+DISM.exe /Online /Remove-Package /PackageName:$updates /quiet /norestart
+# Love, CJ Pollock
